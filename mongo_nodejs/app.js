e99ad910dcc6dce4dbc57c25f94b8e707a26390a
gagan0183 <g.deepsingh1@gmail.com>
2017-08-13 23:25:39 +0530

    changes

--- mongo_nodejs/app.js
+++ mongo_nodejs/app.js
@@ -1,15 +1,37 @@
 var MongoClient = require('mongodb').MongoClient;
     assert = require('assert');
+    express = require('express');
+    app = express();
+    nunjucks = require('nunjucks');
 
-MongoClient.connect('mongodb://localhost:27017/video', function (err, db) {
-    assert.equal(null, err);
-    console.log('Successfully connects');
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
-db.collection('movies').find({}).toArray(function (err, docs) {  
-        docs.forEach(function (doc) {
-            console.log(doc.title);
+app.get('/movies', function (req, res) {
+    MongoClient.connect('mongodb://localhost:27017/video', function (err, db) {
+        assert.equal(null, err);
+        console.log('Successfully connects');
+
+        db.collection('movies').find({}).toArray(function (err, docs) {  
+                res.render('movies', {'movies': docs});
+                db.close();
+            });
         });
+ });   
 
-        db.close();
-    });
+app.get('/', function (req, res) {
+    res.render('templ', {'name': 'templates'}); 
+});
+
+
+app.use(function (req, res) {
+    res.send(404); 
+});
+
+app.listen(3000, function (req, res) {
+        console.log('server listening on 3000'); 
 });
